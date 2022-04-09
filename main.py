286dc0ff0c931c7f14b56ea3c9e92ef308254451
Struan Duncan-Wilson <struan@duncan-wilson.co.uk>
2022-04-09 14:28:09 +0100

    Ignore environment dirs

---
+++ main.py
@@ -0,0 +1,45 @@
+import karelia
+import pprint
+
+present_bot = karelia.bot(["Present", "present"], "xkcd")
+
+present_bot.stock_responses["short_help"] = "I track attendence"
+present_bot.stock_responses[
+    "long_help"
+] = "I keep a running, accurate tally of which people are connected. (The tally converges closer to accurate with both time and activity.) Use !present to find out. Made by @PouncySilverkitten."
+
+present_bot.connect(stealth=True)
+present = {}
+
+user_list = []
+bot_list = []
+last_nick = "Present"
+
+while True:
+    message = present_bot.parse()
+    #pprint.pprint(message.packet)
+    try:
+        if message.type == "nick-event":
+            present[message.data.id] = message.data.to
+        elif message.type == "join-event":
+            present[message.data.id] = message.data.name
+        elif message.type == "send-event":
+            present[message.data.sender.id] = message.data.sender.name
+        elif message.type == "part-event":
+            del present[message.data.id]
+    except KeyError:
+        pass
+
+    user_list = sorted([present[key] for key in present.keys() if not key.startswith("bot:")], key = lambda x: x.lower())
+    bot_list = sorted([present[key] for key in present.keys() if key.startswith("bot:")])
+    
+    if message.type == "send-event" and message.data.content == "!present":
+        users = '\n'.join(user_list)
+        bots = '\n'.join(bot_list)
+        #present_bot.reply(f"{users}\n\n--------------------\n\n{bots}")
+        print(users)
+
+    this_nick = f"Present ({len(user_list)}|{len(bot_list)})"
+    if this_nick != last_nick:
+        #present_bot.change_nick(this_nick)
+        last_nick = this_nick
